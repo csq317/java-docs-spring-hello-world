3f480f5962c358905493fefaae6eef90e996647b
biyuanchun <116139421+biyuanchun@users.noreply.github.com>
2022-10-24 12:18:10 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/")
+	@RequestMapping("/byc")
 	String sayHello() {
 		return "Hello byc!";
 	}
