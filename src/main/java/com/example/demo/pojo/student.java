060e78e5457cec3a78286c432e80cf0f7e2bb463
csq317 <84362463+csq317@users.noreply.github.com>
2022-10-26 15:06:01 +0800

    Update student.java

--- src/main/java/com/example/demo/pojo/student.java
+++ src/main/java/com/example/demo/pojo/student.java
@@ -12,4 +12,36 @@ public class    student {
     private Integer id;
     private String name;
     private String sex;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
 }
