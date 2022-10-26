a6fff834865f05a1253a719da7333f41456865af
biyuanchun <116139421+biyuanchun@users.noreply.github.com>
2022-10-26 09:29:02 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 @SpringBootApplication
 @RestController
@@ -12,8 +16,15 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@RequestMapping("/byc")
-	String sayHello() {
-		return "Hello byc!";
-	}
+	
+   @RequestMapping("/byc")
+  public  String sayHello() throws ClassNotFoundException, SQLException {
+        String driverName = "com.mysql.cj.jdbc.Driver";
+        String DB_URL = "jdbc:mysql://47.98.237.169:3306";
+        String USER = "root";
+        String PASS = "root";
+	Class.forName(driverName);
+	Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	return conn.toString();
+    }
 }
